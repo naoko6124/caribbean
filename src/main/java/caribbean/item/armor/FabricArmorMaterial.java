8438a1209a4bceb1eb53f44bb2784348f5b933fa
Murilo <muzbz09@gmail.com>
2023-06-01 16:39:57 -0300

    Armor deu bom com bug
    botão direito na mão crasha
    falta textura

---
+++ src/main/java/caribbean/item/armor/FabricArmorMaterial.java
@@ -0,0 +1,54 @@
+package caribbean.item.armor;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+
+public class FabricArmorMaterial implements ArmorMaterial {
+    private  static  final  int [] BASE_DURABILITY = new int[]{13,15,16,11};
+    private static final int [] PROTECTION_AMOUNTS = new int[] {2,5,6,3};
+
+
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return BASE_DURABILITY[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return PROTECTION_AMOUNTS [slot.getEntitySlotId()];
+    }
+
+
+    @Override
+    public int getEnchantability() {
+        return 25;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return null;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public float getToughness() {
+        return 0;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0;
+    }
+}
